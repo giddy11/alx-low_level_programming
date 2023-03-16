4dc09753a912521ce777ed6dd28d24bbbb54867f
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-03-16 22:20:49 +0000

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("Size of a char: %c byte(s)");
+	puts("Size of an int: %d byte(s)");
+	puts("Size of a long int: %u byte(s)");
+	puts("Size of a long long int: %lld byte(s)");
+	puts("Size of a float: %f byte(s)");
+	return (0);
+}
