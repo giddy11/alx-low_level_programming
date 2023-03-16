d7011a92d77955a7b181bcf335d5abb74de6be3e
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-03-16 22:23:15 +0000

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	puts("Size of a char: %c byte(s)");
-	puts("Size of an int: %d byte(s)");
-	puts("Size of a long int: %u byte(s)");
-	puts("Size of a long long int: %lld byte(s)");
-	puts("Size of a float: %f byte(s)");
+	puts("Size of a char: %c byte(s)", sizeof(char));
+	puts("Size of an int: %d byte(s)", sizeof(int));
+	puts("Size of a long int: %u byte(s)", sizeof(long int));
+	puts("Size of a long long int: %lld byte(s)", sizeof(long long int));
+	puts("Size of a float: %f byte(s)", sizeof(float));
 	return (0);
 }
