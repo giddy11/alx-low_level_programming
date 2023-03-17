69b849e4e2f5595fdc80e6d2ab9670ad30bea027
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-03-17 22:11:37 +0000

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - The starting of the program
+ * Return: always true (0)
+ *
+ **/
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
