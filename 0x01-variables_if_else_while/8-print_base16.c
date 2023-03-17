4a0661e4abe4942179e583e8bca65c12093f2f85
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-03-17 21:45:50 +0000

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - The starting of the program
+ * Return: always true (0)
+ *
+ **/
+
+int main(void)
+{
+	int num;
+	char i;
+
+	for (num = '0'; num <= '9'; i++)
+		putchar(num);
+
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
