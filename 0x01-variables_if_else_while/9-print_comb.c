0292a31bd821f98add70c31360e8ee78da343ce4
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-03-17 22:22:03 +0000

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar(num);
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('$');
+	putchar('\n');
 
 	return (0);
 }
