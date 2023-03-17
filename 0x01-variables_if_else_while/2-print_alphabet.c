19360a7e08c67d349cec7f6cd1bd9016216cda00
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-03-17 18:38:53 +0000

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - The starting of the program
+ * Return: always true (0)
+ *
+ **/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	
+	return (0);
+}
