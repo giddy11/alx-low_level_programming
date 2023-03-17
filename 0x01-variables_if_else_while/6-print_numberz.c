21857937d1d816ba1927302eb121d2c6626a4fd5
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-03-17 21:35:30 +0000

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - The starting of the program
+ * Return: always true (0)
+ *
+ **/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
