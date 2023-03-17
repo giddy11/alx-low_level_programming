624aab37ec74eaf8314982be49db55aa50da3e00
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-03-17 21:20:42 +0000

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - The starting of the program
+ * Return: always true (0)
+ *
+ **/
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+
+	return (0);
+}
