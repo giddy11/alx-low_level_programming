8721aac18a60ba708ca4f804f385544f21f1e1a4
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-03-17 19:04:47 +0000

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - The starting of the program
+ * Return: always true (0)
+ *
+ **/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+
+	for (i = 65; i <= 90; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
