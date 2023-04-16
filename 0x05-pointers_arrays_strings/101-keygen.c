bd6d979b74a1763f6bf53527c81cadae6797a999
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-04-16 16:21:15 +0000

    task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,10 +7,10 @@ int main(void)
     int i;
     srand(time(NULL));
 
-    for (i = 0; i < 50; i++)
+    for (i = 0; i < 15; i++)
     {
         int k = rand() % 128;
-        if ((k >= 48 && k <= 57) || (k >= 65 && k <= 90) || (k >= 97 && k <= 122) || (k >= 58 && k <= 64))
+        if ((k >= 65 && k <= 90) || (k >= 97 && k <= 122) || (k >= 32 && k <= 33))
         {
             printf("%c", k);
         }
