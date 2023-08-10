c8f9ce220e7f493356570acbed69ec3ed628f558
giddy11 <edoghotugiddy@gmail.com>
2023-08-10 14:00:39 +0000

    solved task

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,17 +8,19 @@
  */
 int *array_range(int min, int max)
 {
-	int *ptr, i;
+	int *ptr, i, tmp;
+
+	tmp = min;
 
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc((max + 1) * sizeof(int));
+	ptr = malloc((max - min + 1) * sizeof(int));
 	if (!ptr)
 		return (NULL);
 
-	for (i = 0; i <= max; i++)
-		ptr[i] = min++;
+	for (i = 0; i <= max - min; i++)
+		ptr[i] = tmp++;
 
 	return (ptr);
 }
