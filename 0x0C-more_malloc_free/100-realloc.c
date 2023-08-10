678f18a96eb29471b95a5764afd6bb74941819ea
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-08-10 19:26:29 +0000

    solved task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,32 +10,35 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *pptr, *rel;
+	char *p, *relloc;
 	unsigned int i;
 
 	if (ptr != NULL)
-		pptr = ptr;
+	p = ptr;
+
 	else
+	{
 		return (malloc(new_size));
+	}
 
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0 && !ptr)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (0);
 	}
 
-	rel = malloc(new_size);
-	if (!rel)
+	relloc = malloc(new_size);
+	if (relloc == NULL)
 		return (0);
 
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		rel[i] = pptr[i];
+		*(relloc + i) = p[i];
 	}
 
 	free(ptr);
-	return (rel);
+	return (relloc);
 }
