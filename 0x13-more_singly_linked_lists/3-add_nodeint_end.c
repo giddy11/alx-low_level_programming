560f08c0a29fcb8528b015fe795eef7e61fe7d22
giddy11 <edoghotugiddy@gmail.com>
2023-08-28 13:25:07 +0000

    corrected

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -29,8 +29,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		ptr = *head;
 		for (; ptr->next != NULL; ptr = ptr->next)
 			;
+		ptr->next = temp;
+
 	}
-	ptr->next = temp;
 
 	return (*head);
 }
