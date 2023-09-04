e64cf6ff00c4462c5c63ea080b014d7bb3bdc7ef
giddy11 <edoghotugiddy@gmail.com>
2023-09-04 17:36:13 +0000

    added

---
+++ 0x15-file_io/0-test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize op, read_only, write_only;
+	char *buff;
+
+	 
+
+
+ 	return (0);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+    ssize_t n;
+
+    if (ac != 2)
+    {
+        dprintf(2, "Usage: %s filename\n", av[0]);
+        exit(1);
+    }
+    n = read_textfile(av[1], 114);
+    printf("\n(printed chars: %li)\n", n);
+    n = read_textfile(av[1], 1024);
+    printf("\n(printed chars: %li)\n", n);
+    return (0);
+}
