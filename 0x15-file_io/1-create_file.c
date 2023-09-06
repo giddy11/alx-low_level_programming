dbef97dd046d513d5fb0d654ac122f53b75190ce
Gideon Azibaobuom Edoghotu <edoghotugiddy@gmail.com>
2023-09-06 12:46:57 +0000

    solved

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			txt_len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	bytes_written = write(fd, text_content, txt_len);
 
 	if (fd == -1 || bytes_written == -1)
