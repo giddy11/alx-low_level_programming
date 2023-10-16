166decb2d1a5f0197071dc7749cff6e1e258abda
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-10-16 15:04:15 +0000

    solved

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ *
+ * @n: int type number
+ *
+ * Return: Absolute value of @n
+ *
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
